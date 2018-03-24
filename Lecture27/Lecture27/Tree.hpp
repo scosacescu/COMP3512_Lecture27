73a614352ec773a5075967324dd29999ac9cdc12
scosacescu <stephanie.cosacescu@gmail.com>
2018-03-24 01:54:31 -0700

    Level order traveral for insertion and print method

--- Lecture27/Lecture27/Tree.hpp
+++ Lecture27/Lecture27/Tree.hpp
@@ -1,15 +1,69 @@
 #pragma once
 #include "Node.hpp"
+#include <queue>
 
 template <typename T = string>
 class Tree {
-private: 
-	
+
 public:
 	Node<T>* root;
 	Tree<T>(T datum){
 		root = new Node<T>(datum);
 	}
-	
 
+	//Insert a new node into the tree based on Level Order Traversal (BFS)
+	//PRE: A value to create a new node from
+	//POST: The tree will have one more node
+	//modified from https://www.geeksforgeeks.org/insertion-binary-tree/
+	void addNode(T datum) {
+		Node<T>* newNode = new Node<T>(datum);			//node to be inserted
+		queue<Node<T>*> queue;							//use a queue to keep track of position/progress
+		queue.push(root);
+
+		while (!queue.empty()) {
+
+			Node<T>* temp = queue.front();
+			queue.pop();
+
+			if (!temp->leftChild) {
+				temp->leftChild = newNode;
+				break;
+			}
+			else
+				queue.push(temp->leftChild);
+
+			if (!temp->rightChild) {
+				temp->rightChild = newNode;
+				break;
+			}
+			else
+				queue.push(temp->rightChild);
+		}
+	}
+
+	//This print method is also based on Level Order traversal
+	//PRE: A tree must exist (aka >= one node to traverse)
+	//POST: The datum at each node in the tree will be displayed 
+	void print() {
+		queue<Node<T>*> queue;
+		queue.push(root);
+
+		while (!queue.empty()) {
+			Node<T>* temp = queue.front();
+			cout << temp->datum.c_str()<<endl;
+			queue.pop();
+
+			if (!temp->leftChild) {
+				//break;
+			}
+			else
+				queue.push(temp->leftChild);
+
+			if (!temp->rightChild) {
+				//break;
+			}
+			else
+				queue.push(temp->rightChild);
+		}
+	}
 };
--- Lecture27/Lecture27/TreeTester.cpp
+++ Lecture27/Lecture27/TreeTester.cpp
@@ -3,15 +3,20 @@
 
 int main() {
 	
-	Node<>* n = new Node<>(string("Hello"));
-	
-	cout << n->datum.c_str() << endl;
+	//Node<>* n = new Node<>(string("Hello"));
+	//cout << n->datum.c_str() << endl;
 
-	//
 
-	Tree<>* myLilTree = new Tree<>(string("Josh is cute"));
-	cout << myLilTree->root->datum.c_str() << endl;
-	
+	Tree<>* myLilTree = new Tree<>(string("Root Node"));
+	//cout << myLilTree->root->datum.c_str() << endl;
+
+	myLilTree->addNode(string("2"));
+	myLilTree->addNode(string("3"));
+	myLilTree->addNode(string("4"));
+	myLilTree->addNode(string("5"));
+	myLilTree->addNode(string("6"));
+	myLilTree->addNode(string("7"));
+	myLilTree->print();
 
 	cin.get();
 	
