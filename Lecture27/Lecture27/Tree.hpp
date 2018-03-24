55807f20b04856c67aea00a964f8f96f71f62d22
scosacescu <stephanie.cosacescu@gmail.com>
2018-03-24 00:17:37 -0700

    Create Tree class template

--- Lecture27/Lecture27/Tree.hpp
+++ Lecture27/Lecture27/Tree.hpp
@@ -1,7 +1,15 @@
 #pragma once
 #include "Node.hpp"
 
-template <typename T>
+template <typename T = string>
 class Tree {
+private: 
+	
+public:
+	Node<T>* root;
+	Tree<T>(T datum){
+		root = new Node<T>(datum);
+	}
+	
 
 };
--- Lecture27/Lecture27/TreeTester.cpp
+++ Lecture27/Lecture27/TreeTester.cpp
@@ -1,12 +1,18 @@
 #include "Tree.hpp"
 #include "Node.hpp"
-using namespace std;
 
 int main() {
-	//Node<>* n1 = new Node<>(string("Hello"));
 	
-	Node<> n = Node<string>("Hello");
-	cout << n.datum.c_str() << endl;
+	Node<>* n = new Node<>(string("Hello"));
+	
+	cout << n->datum.c_str() << endl;
+
+	//
+
+	Tree<>* myLilTree = new Tree<>(string("Josh is cute"));
+	cout << myLilTree->root->datum.c_str() << endl;
+	
+
 	cin.get();
 	
 }
